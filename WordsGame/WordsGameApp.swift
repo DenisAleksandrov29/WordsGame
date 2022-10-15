8b24c72cbc3449ed44c74204f88ccfdfecfec526
DenisA <aie-dinis2007@yandex.ru>
2022-10-15 14:33:06 +0300

    Initial Commit

---
+++ WordsGame/WordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsGameApp.swift
+//  WordsGame
+//
+//  Created by Денис Александров on 15.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
